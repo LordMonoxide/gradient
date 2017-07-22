625f013f729d25cf721d81e784a58d05561bc6b9
Corey Frenette <corey@narwhunderful.com>
2017-07-22 11:06:57 -0300

    Dead imports

--- src/main/java/lordmonoxide/gradient/blocks/claycrucible/GuiClayCrucible.java
+++ src/main/java/lordmonoxide/gradient/blocks/claycrucible/GuiClayCrucible.java
@@ -3,13 +3,11 @@ package lordmonoxide.gradient.blocks.claycrucible;
 import lordmonoxide.gradient.GradientMod;
 import lordmonoxide.gradient.blocks.GradientBlocks;
 import lordmonoxide.gradient.blocks.firepit.ContainerFirePit;
-import lordmonoxide.gradient.blocks.firepit.TileFirePit;
 import lordmonoxide.gradient.containers.GradientContainer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
 
 public class GuiClayCrucible extends GuiContainer {

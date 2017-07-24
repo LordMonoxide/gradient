8c82a9e1c230f25d5f16fc6be3544755c459d164
Corey Frenette <corey@narwhunderful.com>
2017-07-24 20:39:44 -0300

    [#33] Give flint knife 50 uses before breaking (closes #33)

--- src/main/java/lordmonoxide/gradient/items/FlintKnife.java
+++ src/main/java/lordmonoxide/gradient/items/FlintKnife.java
@@ -8,6 +8,7 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 public class FlintKnife extends GradientItemTool implements GradientItemCraftable {
   public FlintKnife() {
     super("flint_knife", 0, -1.0f, 3);
+    this.setMaxDamage(49);
   }
   
   @Override

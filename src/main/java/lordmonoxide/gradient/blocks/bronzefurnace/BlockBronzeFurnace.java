1b3083d7af931353754b72d5a48f4387efac935e
Corey Frenette <corey@narwhunderful.com>
2017-08-16 22:10:01 -0300

    [#87] Start of bronze furnace

---
+++ src/main/java/lordmonoxide/gradient/blocks/bronzefurnace/BlockBronzeFurnace.java
@@ -0,0 +1,77 @@
+package lordmonoxide.gradient.blocks.bronzefurnace;
+
+import lordmonoxide.gradient.GradientGuiHandler;
+import lordmonoxide.gradient.GradientMod;
+import lordmonoxide.gradient.blocks.GradientBlock;
+import lordmonoxide.gradient.blocks.GradientBlocks;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockBronzeFurnace extends GradientBlock {
+  public static final PropertyDirection FACING = BlockHorizontal.FACING;
+  
+  public BlockBronzeFurnace() {
+    super("bronze_furnace", CreativeTabs.TOOLS, GradientBlocks.MATERIAL_BRONZE_MACHINE); //$NON-NLS-1$
+    this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    this.setResistance(5.0f);
+    this.setHardness(1.0f);
+  }
+  
+  @Override
+  public boolean onBlockActivated(final World world, final BlockPos pos, final IBlockState state, final EntityPlayer player, final EnumHand hand, final EnumFacing side, final float hitX, final float hitY, final float hitZ) {
+    if(!world.isRemote) {
+      if(!player.isSneaking()) {
+        player.openGui(GradientMod.instance, GradientGuiHandler.BRONZE_BOILER, world, pos.getX(), pos.getY(), pos.getZ());
+      }
+    }
+    
+    return true;
+  }
+  
+  @Override
+  public void onBlockPlacedBy(final World world, final BlockPos pos, final IBlockState state, final EntityLivingBase placer, final ItemStack stack) {
+    world.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+  }
+  
+  @Override
+  @Deprecated
+  public IBlockState getStateFromMeta(final int meta) {
+    EnumFacing facing = EnumFacing.getHorizontal(meta);
+    
+    return this.getDefaultState().withProperty(FACING, facing);
+  }
+  
+  @Override
+  public int getMetaFromState(final IBlockState state) {
+    return state.getValue(FACING).getHorizontalIndex();
+  }
+  
+  @Override
+  @Deprecated
+  public IBlockState withRotation(final IBlockState state, final Rotation rot) {
+    return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+  }
+  
+  @Override
+  @Deprecated
+  public IBlockState withMirror(final IBlockState state, final Mirror mirror) {
+    return state.withRotation(mirror.toRotation(state.getValue(FACING)));
+  }
+  
+  @Override
+  protected BlockStateContainer createBlockState() {
+    return new BlockStateContainer(this, FACING);
+  }
+}

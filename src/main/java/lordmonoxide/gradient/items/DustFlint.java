ff05995525cbed7074056d2987abd1c4b4e37c2e
Corey Frenette <corey@narwhunderful.com>
2017-08-21 21:58:12 -0300

    [#102] Add recipe for flint dust (closes #102)

--- src/main/java/lordmonoxide/gradient/items/DustFlint.java
+++ src/main/java/lordmonoxide/gradient/items/DustFlint.java
@@ -1,9 +1,21 @@
 package lordmonoxide.gradient.items;
 
+import lordmonoxide.gradient.recipes.GradientCraftable;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class DustFlint extends GradientItem {
+public class DustFlint extends GradientItem implements GradientCraftable {
   public DustFlint() {
     super("dust.flint", CreativeTabs.MATERIALS);
+  }
+  
+  @Override
+  public void addRecipe() {
+    GameRegistry.addShapelessRecipe(
+      this.getItemStack(),
+      Items.FLINT,
+      GradientItems.MORTAR
+    );
   }
 }

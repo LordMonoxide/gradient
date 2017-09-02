718c5defc674b90245c47049247c276ed17f8b89
Corey Frenette <corey@narwhunderful.com>
2017-09-02 09:24:01 -0300

    [#118] More recipes

--- src/main/java/lordmonoxide/gradient/items/StoneHammer.java
+++ src/main/java/lordmonoxide/gradient/items/StoneHammer.java
@@ -1,29 +1,10 @@
 package lordmonoxide.gradient.items;
 
-import lordmonoxide.gradient.blocks.GradientBlocks;
-import lordmonoxide.gradient.recipes.GradientCraftable;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-
-public class StoneHammer extends GradientItemTool implements GradientCraftable {
+public class StoneHammer extends GradientItemTool {
   public StoneHammer() {
     super("stone_hammer", 0.5f, -2.4f, 2);
     this.setHarvestLevel("pickaxe", 2);
     this.setHarvestLevel("hammer", 2);
     this.setMaxDamage(19);
-  }
-  
-  @Override
-  public void addRecipe() {
-    //TODO
-    /*GameRegistry.addRecipe(new ShapedOreRecipe(
-      this,
-      "P",
-      "F",
-      "S",
-      'P', GradientBlocks.PEBBLE,
-      'F', "string",
-      'S', "stickWood"
-    ));*/
   }
 }
--- src/main/java/lordmonoxide/gradient/items/StoneMattock.java
+++ src/main/java/lordmonoxide/gradient/items/StoneMattock.java
@@ -1,7 +1,5 @@
 package lordmonoxide.gradient.items;
 
-import lordmonoxide.gradient.blocks.GradientBlocks;
-import lordmonoxide.gradient.recipes.GradientCraftable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.util.EnumActionResult;
@@ -9,10 +7,8 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.ShapedOreRecipe;
 
-public class StoneMattock extends GradientItemTool implements GradientCraftable {
+public class StoneMattock extends GradientItemTool {
   public StoneMattock() {
     super("stone_mattock", 0.5f, -2.4f, 4);
     this.setHarvestLevel("axe", 0);
@@ -23,18 +19,5 @@ public class StoneMattock extends GradientItemTool implements GradientCraftable 
   @Override
   public EnumActionResult onItemUse(final EntityPlayer playerIn, final World worldIn, final BlockPos pos, final EnumHand hand, final EnumFacing facing, final float hitX, final float hitY, final float hitZ) {
     return Items.STONE_HOE.onItemUse(playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
-  }
-  
-  @Override
-  public void addRecipe() {
-    //TODO
-    /*GameRegistry.addRecipe(new ShapedOreRecipe(
-      this,
-      "PFP",
-      " S ",
-      'P', GradientBlocks.PEBBLE,
-      'F', "string",
-      'S', "stickWood"
-    ));*/
   }
 }

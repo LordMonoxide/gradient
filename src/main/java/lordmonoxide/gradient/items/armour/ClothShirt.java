30ad0a281cb6ac2a29a557ccc5c40da0041827f5
Corey Frenette <corey@narwhunderful.com>
2017-09-02 13:51:59 -0300

    [#118] More

--- src/main/java/lordmonoxide/gradient/items/armour/ClothShirt.java
+++ src/main/java/lordmonoxide/gradient/items/armour/ClothShirt.java
@@ -1,27 +1,10 @@
 package lordmonoxide.gradient.items.armour;
 
-import lordmonoxide.gradient.recipes.GradientCraftable;
 import lordmonoxide.gradient.items.GradientItems;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.ShapedOreRecipe;
 
-public class ClothShirt extends GradientArmour implements GradientCraftable {
+public class ClothShirt extends GradientArmour {
   public ClothShirt() {
     super("cloth_shirt", GradientItems.MATERIAL_CLOTH, 0, EntityEquipmentSlot.CHEST);
-  }
-  
-  @Override
-  public void addRecipe() {
-    //TODO
-    /*GameRegistry.addRecipe(new ShapedOreRecipe(
-      this,
-      "CNC",
-      "CSC",
-      "CCC",
-      'C', "cloth",
-      'N', "needle",
-      'S', "string"
-    ));*/
   }
 }

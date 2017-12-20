ca52af879f92017c62d6c3ca36b987ad6a0b275b
Corey Frenette <corey@narwhunderful.com>
2017-12-20 18:01:43 -0400

    [#164] Add tooltips for casts (closes #164)

--- src/main/java/lordmonoxide/gradient/blocks/claycast/BlockClayCast.java
+++ src/main/java/lordmonoxide/gradient/blocks/claycast/BlockClayCast.java
@@ -5,10 +5,19 @@ import lordmonoxide.gradient.blocks.GradientBlocks;
 import lordmonoxide.gradient.GradientCasts;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BlockClayCast extends GradientBlock {
   private static final AxisAlignedBB AABB = new AxisAlignedBB(0.0d, 0.0d, 0.0d, 1.0d, 2.0d / 16.0d, 1.0d);
@@ -20,6 +29,12 @@ public class BlockClayCast extends GradientBlock {
     this.setDefaultState(this.blockState.getBaseState().withProperty(CAST, GradientCasts.PICKAXE));
     this.setResistance(5.0f);
     this.setHardness(1.0f);
+  }
+  
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void addInformation(final ItemStack stack, @Nullable final World player, final List<String> tooltip, final ITooltipFlag advanced) {
+    tooltip.add(I18n.format("tile.clay_cast.metal_amount", this.getStateFromMeta(stack.getMetadata()).getValue(CAST).amount));
   }
   
   @Override

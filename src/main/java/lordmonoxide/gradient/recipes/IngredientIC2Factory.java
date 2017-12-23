c1c1207d34e467770c2e292ccf985e5f13936e6f
Corey Frenette <corey@narwhunderful.com>
2017-12-22 20:47:26 -0400

    [#170] Didn't commit new file

---
+++ src/main/java/lordmonoxide/gradient/recipes/IngredientIC2Factory.java
@@ -0,0 +1,30 @@
+package lordmonoxide.gradient.recipes;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import ic2.api.item.IC2Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+public class IngredientIC2Factory implements IIngredientFactory {
+  @Override
+  public Ingredient parse(final JsonContext context, final JsonObject json) {
+    final String name = JsonUtils.getString(json, "name");
+    final String variant = JsonUtils.getString(json, "variant", null);
+    
+    final ItemStack stack = IC2Items.getItem(name, variant);
+    
+    if(stack == null || stack.isEmpty()) {
+      if(variant == null) {
+        throw new JsonSyntaxException("No IC2 item " + name);
+      }
+      
+      throw new JsonSyntaxException("No IC2 item " + name + ':' + variant);
+    }
+    
+    return Ingredient.fromStacks(stack);
+  }
+}

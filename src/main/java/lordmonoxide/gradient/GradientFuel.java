e6198de9e3667395f81c40ae6496b6912244c0b0
Corey Frenette <corey@narwhunderful.com>
2018-10-10 11:42:01 -0300

    [#288] Twine

--- src/main/java/lordmonoxide/gradient/GradientFuel.java
+++ src/main/java/lordmonoxide/gradient/GradientFuel.java
@@ -16,7 +16,8 @@ public final class GradientFuel {
 
   static {
     add("igniter",        5,  50,  500, 32.00f);
-    add("string",         5,  50,  125, 18.00f);
+    add("fibre",          5,  50,  125, 18.00f);
+    add("string",        20,  50,  125, 18.00f);
     add("stickWood",     30, 100,  350,  2.16f);
     add("treeSapling",   60, 150,  350,  1.38f);
     add("plankWood",    600, 230,  750,  1.04f);
---
+++ src/main/java/lordmonoxide/gradient/items/Twine.java
@@ -0,0 +1,9 @@
+package lordmonoxide.gradient.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class Twine extends GradientItem {
+  public Twine() {
+    super("twine", CreativeTabs.MATERIALS);
+  }
+}

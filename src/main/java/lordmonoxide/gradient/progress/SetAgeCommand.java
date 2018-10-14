9cc629b46a83072f73d3b5a0d849bcf8c631e70c
Corey Frenette <corey@narwhunderful.com>
2018-10-14 01:45:11 -0300

    [#308] Finish setage command

--- src/main/java/lordmonoxide/gradient/progress/SetAgeCommand.java
+++ src/main/java/lordmonoxide/gradient/progress/SetAgeCommand.java
@@ -3,8 +3,10 @@ package lordmonoxide.gradient.progress;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 
 public class SetAgeCommand extends CommandBase {
   @Override
@@ -14,22 +16,41 @@ public class SetAgeCommand extends CommandBase {
 
   @Override
   public String getUsage(final ICommandSender sender) {
-    return "setusage";
+    return "commands.setage.usage";
   }
 
   @Override
   public void execute(final MinecraftServer server, final ICommandSender sender, final String[] args) throws CommandException {
-    if(sender.getCommandSenderEntity() == null) {
-      return;
+    if(args.length < 1) {
+      throw new WrongUsageException("commands.setage.usage");
     }
 
-    final PlayerProgress progress = sender.getCommandSenderEntity().getCapability(CapabilityPlayerProgress.PLAYER_PROGRESS_CAPABILITY, null);
+    final Age age = Age.get(CommandBase.parseInt(args[0], 1, Age.values().length));
+
+    final Entity target;
+
+    if(args.length == 2) {
+      target = CommandBase.getEntity(server, sender, args[1]);
+    } else {
+      if(sender.getCommandSenderEntity() == null) {
+        throw new WrongUsageException("commands.setage.usage");
+      }
+
+      target = sender.getCommandSenderEntity();
+    }
+
+    final PlayerProgress progress = target.getCapability(CapabilityPlayerProgress.PLAYER_PROGRESS_CAPABILITY, null);
 
     if(progress == null) {
       return;
     }
 
-    progress.setAge(Age.values()[(progress.getAge().ordinal() + 1) % 4]);
-    sender.sendMessage(new TextComponentString("Set age to " + progress.getAge()));
+    progress.setAge(age);
+
+    sender.sendMessage(new TextComponentTranslation("commands.setage.set", target.getDisplayName(), age));
+
+    if(sender != target) {
+      sender.sendMessage(new TextComponentTranslation("commands.setage.set", age));
+    }
   }
 }

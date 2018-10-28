c62d47693c3693f5c49024a738b82e8f069c5b07
Corey Frenette <corey@narwhunderful.com>
2018-10-28 15:37:11 -0300

    [#327] Waterskins (closes #327)

--- src/main/java/lordmonoxide/gradient/items/GradientItems.java
+++ src/main/java/lordmonoxide/gradient/items/GradientItems.java
@@ -285,6 +285,7 @@ public final class GradientItems {
       RegistrationHandler.register(new GradientArmour("hide_headcover", MATERIAL_HIDE, 0, EntityEquipmentSlot.HEAD));
 
       RegistrationHandler.register(new HideBedding());
+      RegistrationHandler.register(new Waterskin());
 
       // Trigger item registration
       new GradientItems();

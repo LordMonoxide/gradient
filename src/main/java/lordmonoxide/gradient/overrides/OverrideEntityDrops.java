e2d34791672ed6d4f3dd8c9df5f881860adda47a
Corey Frenette <corey@narwhunderful.com>
2018-11-10 21:10:36 -0400

    [#408] Fix drops (closes #408)

--- src/main/java/lordmonoxide/gradient/overrides/OverrideEntityDrops.java
+++ src/main/java/lordmonoxide/gradient/overrides/OverrideEntityDrops.java
@@ -1,6 +1,7 @@
 package lordmonoxide.gradient.overrides;
 
 import lordmonoxide.gradient.GradientMod;
+import lordmonoxide.gradient.items.GradientItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.AbstractIllager;
@@ -12,11 +13,13 @@ import net.minecraft.entity.monster.EntityWitch;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -24,38 +27,8 @@ import java.util.Iterator;
 import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = GradientMod.MODID)
-@GameRegistry.ObjectHolder("gradient")
 public final class OverrideEntityDrops {
   private OverrideEntityDrops() { }
-
-  private static final Item HIDE_COW = null;
-  private static final Item HIDE_DONKEY = null;
-  private static final Item HIDE_HORSE = null;
-  private static final Item HIDE_LLAMA = null;
-  private static final Item HIDE_MULE = null;
-  private static final Item HIDE_OCELOT = null;
-  private static final Item HIDE_PIG = null;
-  private static final Item HIDE_POLAR_BEAR = null;
-  private static final Item HIDE_SHEEP = null;
-  private static final Item HIDE_WOLF = null;
-
-  @GameRegistry.ObjectHolder("minecraft:bone")
-  private static final Item BONE = null;
-
-  @GameRegistry.ObjectHolder("minecraft:string")
-  private static final Item STRING = null;
-
-  @GameRegistry.ObjectHolder("minecraft:leather")
-  private static final Item LEATHER = null;
-
-  @GameRegistry.ObjectHolder("minecraft:bow")
-  private static final Item BOW = null;
-
-  @GameRegistry.ObjectHolder("minecraft:arrow")
-  private static final Item ARROW = null;
-
-  @GameRegistry.ObjectHolder("minecraft:iron_ingot")
-  private static final Item IRON_INGOT = null;
 
   @GameRegistry.ObjectHolder("quark:tallow")
   private static final Item TALLOW = null;
@@ -67,7 +40,7 @@ public final class OverrideEntityDrops {
       for(final Iterator<EntityItem> dropIterator = event.getDrops().iterator(); dropIterator.hasNext(); ) {
         final ItemStack drop = dropIterator.next().getItem();
 
-        if(drop.getItem() == LEATHER) {
+        if(drop.getItem() == Items.LEATHER) {
           dropIterator.remove();
         }
       }
@@ -88,52 +61,52 @@ public final class OverrideEntityDrops {
     }
 
     if(entity instanceof EntityCow) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_COW, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_COW, amount)));
       return;
     }
 
     if(entity instanceof EntityDonkey) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_DONKEY, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_DONKEY, amount)));
       return;
     }
 
     if(entity instanceof EntityHorse) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_HORSE, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_HORSE, amount)));
       return;
     }
 
     if(entity instanceof EntityLlama) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_LLAMA, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_LLAMA, amount)));
       return;
     }
 
     if(entity instanceof EntityMule) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_MULE, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_MULE, amount)));
       return;
     }
 
     if(entity instanceof EntityOcelot) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_OCELOT, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_OCELOT, amount)));
       return;
     }
 
     if(entity instanceof EntityPig) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_PIG, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_PIG, amount)));
       return;
     }
 
     if(entity instanceof EntityPolarBear) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_POLAR_BEAR, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_POLAR_BEAR, amount)));
       return;
     }
 
     if(entity instanceof EntitySheep) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_SHEEP, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_SHEEP, amount)));
       return;
     }
 
     if(entity instanceof EntityWolf) {
-      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(HIDE_WOLF, amount)));
+      event.getDrops().add(new EntityItem(entity.world, x, y, z, new ItemStack(GradientItems.HIDE_WOLF, amount)));
       return;
     }
   }
@@ -144,7 +117,7 @@ public final class OverrideEntityDrops {
       for(final Iterator<EntityItem> dropIterator = event.getDrops().iterator(); dropIterator.hasNext(); ) {
         final ItemStack drop = dropIterator.next().getItem();
 
-        if(drop.getItem() == STRING) {
+        if(drop.getItem() == Items.STRING) {
           dropIterator.remove();
         }
       }
@@ -157,7 +130,7 @@ public final class OverrideEntityDrops {
       for(final Iterator<EntityItem> dropIterator = event.getDrops().iterator(); dropIterator.hasNext(); ) {
         final ItemStack drop = dropIterator.next().getItem();
 
-        if(drop.getItem() == IRON_INGOT) {
+        if(drop.getItem() == Items.IRON_INGOT) {
           dropIterator.remove();
         }
       }
@@ -167,11 +140,11 @@ public final class OverrideEntityDrops {
   @SubscribeEvent
   public static void skeletonsOnlyDropArrowsIfTheyHaveBows(final LivingDropsEvent event) {
     if(event.getEntity() instanceof EntitySkeleton) {
-      if(event.getEntityLiving().getHeldItemMainhand().getItem() != BOW) {
+      if(event.getEntityLiving().getHeldItemMainhand().getItem() != Items.BOW) {
         for(final Iterator<EntityItem> dropIterator = event.getDrops().iterator(); dropIterator.hasNext(); ) {
           final ItemStack drop = dropIterator.next().getItem();
 
-          if(drop.getItem() == ARROW) {
+          if(drop.getItem() == Items.ARROW) {
             dropIterator.remove();
           }
         }
@@ -179,7 +152,7 @@ public final class OverrideEntityDrops {
     }
   }
 
-  @SubscribeEvent
+  @SubscribeEvent(priority = EventPriority.LOWEST)
   public static void noTallow(final LivingDropsEvent event) {
     for(final Iterator<EntityItem> dropIterator = event.getDrops().iterator(); dropIterator.hasNext(); ) {
       final ItemStack drop = dropIterator.next().getItem();
@@ -218,7 +191,7 @@ public final class OverrideEntityDrops {
         }
       }
 
-      event.getDrops().add(new EntityItem(world, entity.posX, entity.posY, entity.posZ, new ItemStack(BONE, amount)));
+      event.getDrops().add(new EntityItem(world, entity.posX, entity.posY, entity.posZ, new ItemStack(Items.BONE, amount)));
     }
   }
 }

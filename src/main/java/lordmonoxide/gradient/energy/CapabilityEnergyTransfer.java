5b9f64de30976a4a3fe45cb06cc189367dcc0084
Corey Frenette <corey@narwhunderful.com>
2018-12-16 14:30:41 -0400

    [#478] Transfer nodes do not need to store any data

--- src/main/java/lordmonoxide/gradient/energy/CapabilityEnergyTransfer.java
+++ src/main/java/lordmonoxide/gradient/energy/CapabilityEnergyTransfer.java
@@ -1,7 +1,6 @@
 package lordmonoxide.gradient.energy;
 
 import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagFloat;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -13,16 +12,12 @@ public final class CapabilityEnergyTransfer {
     CapabilityManager.INSTANCE.register(IEnergyTransfer.class, new Capability.IStorage<IEnergyTransfer>() {
       @Override
       public NBTBase writeNBT(final Capability<IEnergyTransfer> capability, final IEnergyTransfer instance, final EnumFacing side) {
-        return new NBTTagFloat(0.0f); // TODO
+        return null;
       }
 
       @Override
       public void readNBT(final Capability<IEnergyTransfer> capability, final IEnergyTransfer instance, final EnumFacing side, final NBTBase nbt) {
-        if(!(instance instanceof EnergyTransfer)) {
-          throw new IllegalArgumentException("Can not deserialize to an instance that isn't the default implementation");
-        }
 
-        //TODO ((EnergyTransfer)instance).energy = ((NBTPrimitive)nbt).getFloat();
       }
     },
     EnergyTransfer::new);

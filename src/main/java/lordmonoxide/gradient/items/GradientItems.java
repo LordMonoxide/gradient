314e4e20d6a191e34876cce7b86a2281cac300d5
Corey Frenette <corey@narwhunderful.com>
2019-04-01 17:11:46 -0300

    [#517] A bunch more triumphs

--- src/main/java/lordmonoxide/gradient/items/GradientItems.java
+++ src/main/java/lordmonoxide/gradient/items/GradientItems.java
@@ -388,9 +388,11 @@ public final class GradientItems {
     for(final GradientMetals.Metal metal : GradientMetals.metals) {
       final String caps = StringUtils.capitalize(metal.name);
 
+      OreDictionary.registerOre("ore", ForgeRegistries.BLOCKS.getValue(GradientMod.resource("ore." + metal.name)));
       OreDictionary.registerOre("ore" + caps, ForgeRegistries.BLOCKS.getValue(GradientMod.resource("ore." + metal.name)));
 
       if(metal.canMakeNuggets) {
+        OreDictionary.registerOre("nugget", nugget(metal));
         OreDictionary.registerOre("nugget" + caps, nugget(metal));
         OreDictionary.registerOre("crushed" + caps, crushed(metal));
         OreDictionary.registerOre("purified" + caps, purified(metal));
